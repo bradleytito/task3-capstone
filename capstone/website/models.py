24cb7f991b2af36e8edf4aaf8f1fe454f206dc70
Bradley Tito <brad.tito@hotmail.com>
2023-08-18 17:45:48 +0200

    added docstrings for models

--- capstone/website/models.py
+++ capstone/website/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Endorser(models.Model):
+    """This class is used to track the names and surnames of all endorsers."""
+
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
 
     def __str__(self):
+        """This method will be used to return the name and surname of a specific endorser saved in the database."""
         return f'{self.name} {self.surname}'
